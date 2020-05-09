3ffed805cb0c55d976a00f5ce0453a782a0125df
yipan Cloud server <yipann@163.com>
2020-05-09 22:20:27 +0800

    hello

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void function();
+
+int main()
+{
+    cout<< "hello, world!" <<endl;
+    function();
+    return 0;
+}
+
+void function()
+{
+    cout<< "function" <<endl;
+}
